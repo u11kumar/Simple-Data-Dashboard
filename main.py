c22c2b2fb255b15fb88c0ab1e6baacbb697f23b4
u11kumar <47977758+u11kumar@users.noreply.github.com>
2024-08-22 15:44:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
+st.title("Simple Data Dashbaord")
+
+uploaded_file=st.file_uploader("Choose a CSV File",type="csv")
+
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+
+    st.subheader("Data Preview")
+    st.write(df.head())
+
+    st.subheader("Data Summary")
+    st.write(df.describe())
+
+    st.subheader("Filter Data")
+    columns=df.columns.tolist()
+    selected_columns=st.selectbox("Select column to filter by",columns)
+    unique_values=df[selected_columns].unique()
+    selected_value=st.selectbox("Select Value",unique_values)
+
+    filtered_df=df[df[selected_columns]==selected_value]
+    st.write(filtered_df)
+
+    st.subheader("Plot Data")
+    x_column=st.selectbox("Select x-axis column",columns)
+    y_column=st.selectbox("Select y-axis column",columns)
+
+    if st.button("Generate Plot"):
+        st.line_chart(filtered_df.set_index(x_column)[y_column])
+    else:
+        st.write("Waiting on file uploaded..")
+    
+
+
+
+
+
+
